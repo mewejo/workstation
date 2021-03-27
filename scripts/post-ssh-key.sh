65d3eca70c7dcc9ee8d87c91f7b6baab3d3327c7
Josh Bonfield <josh@grizzlyware.com>
2021-03-27 20:20:59 +0000

    fix: use ssh

--- scripts/post-ssh-key.sh
+++ scripts/post-ssh-key.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git clone --bare https://github.com/mewejo/dotfiles.git $HOME/.dotfiles;
+git clone --bare git@github.com:mewejo/dotfiles.git $HOME/.dotfiles;
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout;
