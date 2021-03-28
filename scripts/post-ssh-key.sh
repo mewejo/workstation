f331d68536e8d94daf0c4faf08e1541de496c88f
Josh Bonfield <josh@grizzlyware.com>
2021-03-28 10:50:57 +0100

    fix: set local repo to hide untracked

--- scripts/post-ssh-key.sh
+++ scripts/post-ssh-key.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 git clone --bare git@github.com:mewejo/dotfiles.git $HOME/.dotfiles;
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no;
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout;
