a221354026da856436b245f1d63a71d924ecc90f
Josh Bonfield <josh@grizzlyware.com>
2021-03-28 10:55:33 +0100

    fix: set upstream

--- scripts/post-ssh-key.sh
+++ scripts/post-ssh-key.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 git clone --bare git@github.com:mewejo/dotfiles.git $HOME/.dotfiles;
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no;
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout;
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+dotfiles config --local status.showUntrackedFiles no;
+dotfiles checkout;
+dotfiles push --set-upstream origin master;
